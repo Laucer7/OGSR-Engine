afd96192ca10dfa09096b4bc6fee743d438ddcc2
Laucer7 <zagrololo@gmail.com>
2021-03-21 14:20:15 +0100

    Добавил проверку на блокирование слота

--- ogsr_engine/xrGame/Inventory.h
+++ ogsr_engine/xrGame/Inventory.h
@@ -154,6 +154,7 @@ public:
 	void				InvalidateState				()							{ m_dwModifyFrame = Device.dwFrame; }
 	void				Items_SetCurrentEntityHud	(bool current_entity);
 	bool				isBeautifulForActiveSlot	(CInventoryItem *pIItem);
+	bool				IsActiveSlotBlocked()									{ return m_slots[m_iActiveSlot].IsBlocked();  }
 
 	// максимальный вес инвентаря
 	float				m_fMaxWeight;
