a538f6f08a134b124fbf7b0a57471309c9cbdc58
Laucer7 <zagrololo@gmail.com>
2021-05-18 20:43:51 +0200

    Исправлен конфликт в WeaponMagazined.h

--- ogsr_engine/xrGame/WeaponMagazined.h
+++ ogsr_engine/xrGame/WeaponMagazined.h
@@ -106,8 +106,7 @@ public:
 	virtual void	InitZoomParams(LPCSTR section, bool useTexture);
 
 	virtual bool	Action			(s32 cmd, u32 flags);
-	virtual void	onMovementChanged	(ACTOR_DEFS::EMoveCommand cmd);
-	virtual void	UnloadMagazine	(bool spawn_ammo = true);
+	virtual bool	UnloadMagazine	(bool spawn_ammo = true);
 
 	virtual void	GetBriefInfo				(xr_string& str_name, xr_string& icon_sect_name, xr_string& str_count);
 
